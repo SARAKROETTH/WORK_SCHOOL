b8f760602eae055de507912c1a0dbb921a06d9fa
hongXDD <channmey26@gmail.com>
2025-05-19 16:30:15 +0700

    update -- activity with Recyclerview

--- app/src/main/java/com/example/work_school/LogInActivity.java
+++ app/src/main/java/com/example/work_school/LogInActivity.java
@@ -2,8 +2,11 @@ package com.example.work_school;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,11 +14,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.work_school.databinding.ActivityLogInBinding;
 import com.example.work_school.databinding.ActivityWelcomeBinding;
+import com.example.work_school.model.Expense;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
     private ActivityLogInBinding binding;
+
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -24,6 +33,8 @@ public class LogInActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         binding = ActivityLogInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
 
 //         Binding action
         binding.signUpTextView.setOnClickListener(v -> {
@@ -36,7 +47,28 @@ public class LogInActivity extends AppCompatActivity {
     }
 
     private void StartLogin() {
+        binding.pbLoginProgress.setVisibility(View.VISIBLE);
+        String email = binding.inputEmail.getText().toString();
+        String password = binding.inputPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    reload();
+                }else {
 
+                    Toast.makeText(LogInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void reload() {
+        binding.pbLoginProgress.setVisibility(View.GONE);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void StartSignUp() {
---
+++ app/src/main/java/com/example/work_school/model/Expense.java
@@ -0,0 +1,41 @@
+package com.example.work_school.model;
+
+import java.util.Date;
+
+public class Expense {
+
+    private String id;
+    private int amount;
+
+    private String createdBy;
+
+    private String Remark;
+
+    private String category;
+
+    private String Currency;
+
+    private Date createdDate;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
