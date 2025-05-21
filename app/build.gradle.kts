1a57eb1a668399f30e455866bfb2591d05534ad7
hongXDD <channmey26@gmail.com>
2025-05-22 04:28:59 +0700

    update -- activity with Recyclerview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,10 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.airbnb.android:lottie:6.1.0")
+    
+    val preference_version = "1.2.1"
+    implementation("androidx.preference:preference:$preference_version")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/work_school/fragment/SettingFragment.java
+++ app/src/main/java/com/example/work_school/fragment/SettingFragment.java
@@ -2,8 +2,11 @@ package com.example.work_school.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,16 +14,31 @@ import android.view.ViewGroup;
 import com.example.work_school.R;
 import com.example.work_school.databinding.FragmentHomeBinding;
 import com.example.work_school.databinding.FragmentSettingBinding;
+import com.example.work_school.repository.ExpenseRepository;
 
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends PreferenceFragmentCompat {
 
     private FragmentSettingBinding binding;
+
+    ExpenseRepository repository;
+
+
 
 
 
     public SettingFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle bundle, @Nullable String s) {
+        setPreferencesFromResource(R.xml.preferences, s);
+
+        repository = new ExpenseRepository();
+
+
+
     }
 
     @Override
